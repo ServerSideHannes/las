a104504970927881345b945a57c63dd48bc2e8a0
Hannes Gustafsson <ghannes@student.chalmers.se>
2020-01-27 14:58:07 +0100

    Update model.py
    Return sequence enabled and fixed notations for variables

--- model.py
+++ model.py
@@ -5,20 +5,22 @@ class attention(tf.keras.layers.Layer):
   def __init__(self, dim):
     super(attention, self).__init__()
     
-    self.dim = dim
-    self.dense_s = tf.keras.layers.Dense(self.dim)
+    self.dim      = dim
+    self.dense_s  = tf.keras.layers.Dense(self.dim)
+    self.dense_h  = tf.keras.layers.Dense(self.dim)
     
   def call(self, inputs):
     #Split inputs into attentions vectors and inputs from the LSTM output
-    lstm_out      = inputs[0]
-    attention_vec = inputs[1]
+    s     = inputs[0]
+    h     = inputs[1]
     
     #Linear FC
-    lstm_out = self.dense_s(lstm_out)
+    s     = self.dense_s(s)
+    h     = self.dense_h(h)
     
     #Linear blendning
-    alpha = tf.keras.backend.expand_dims(lstm_out)
-    alpha = tf.matmul(attention_vec, alpha)
+    alpha = tf.keras.backend.expand_dims(s)
+    alpha = tf.matmul(h, alpha)
     alpha = tf.keras.backend.squeeze(alpha, axis=-1)
 
     #softmax_vector
@@ -26,10 +28,10 @@ class attention(tf.keras.layers.Layer):
     softmaxed = tf.keras.backend.expand_dims(softmaxed, axis=-2)
 
     #Wheighted vector fetures
-    y = tf.matmul(softmaxed, attention_vec)
-    y = tf.keras.backend.squeeze(y, axis=-2)
+    c = tf.matmul(softmaxed, h)
+    c = tf.keras.backend.squeeze(c, axis=-2)
 
-    return y
+    return c
 
 class att_rnn( tf.keras.layers.Layer):
   def __init__(self, units,):
@@ -37,20 +39,20 @@ class att_rnn( tf.keras.layers.Layer):
     self.units      = units
     self.state_size = [self.units, self.units]
     
-    self.attention_cell = attention(self.units)
-    self.rnn            = tf.keras.layers.LSTMCell(self.units)
-    self.rnn2           = tf.keras.layers.LSTMCell(self.units)
+    self.attention_context  = attention(self.units)
+    self.rnn                = tf.keras.layers.LSTMCell(self.units)
+    self.rnn2               = tf.keras.layers.LSTMCell(self.units)
     
   def call(self, inputs, states, constants):
-    constants = tf.squeeze(constants, axis=0)
+    h       = tf.squeeze(constants, axis=0)
 
-    r         = self.rnn(inputs=inputs, states=states)[1]
-    r         = self.rnn2(inputs=r[0], states=[r[0], r[1]])[1]
+    s       = self.rnn(inputs=inputs, states=states)
+    s       = self.rnn2(inputs=s[0], states=s[1])[1]
+
+    c       = self.attention_context([s[0], h])
+    out     = tf.keras.layers.concatenate([s[0], c], axis=-1)
     
-    c         = self.attention_cell([r[0], constants])
-    c         = tf.add(r[1], c)
-    
-    return r[0], [r[0], c]
+    return out, [s[0], c]
 
 class pBLSTM(tf.keras.layers.Layer):
   def __init__(self, dim):
@@ -60,7 +62,7 @@ class pBLSTM(tf.keras.layers.Layer):
     self.LSTM       = tf.keras.layers.LSTM(self.dim, return_sequences=True)
     self.bidi_LSTM  = tf.keras.layers.Bidirectional(self.LSTM)
     
-  @tf.function
+  #tf.function
   def call(self, inputs):
     y = self.bidi_LSTM(inputs)
     
@@ -81,14 +83,11 @@ def LAS(dim, f_1, no_tokens):
   x = pBLSTM( int(dim/2) )(x)
   x = pBLSTM( int(dim/2) )(x)
   
-  x = tf.keras.layers.Dense(dim,)(x)
-  x = tf.keras.layers.RNN(att_rnn(dim), return_state=True)(input_2, constants=x)
-
-  x = tf.keras.layers.concatenate([x[1], x[2]], axis=-1)
+  x = tf.keras.layers.RNN(att_rnn(dim), return_sequences=True)(input_2, constants=x)
   x = tf.keras.layers.Dense(dim, activation="relu")(x)
   x = tf.keras.layers.Dense(no_tokens, activation="softmax")(x)
 
-  model = tf.keras.Model(inputs=[input_1,input_2] , outputs=x)
+  model = tf.keras.Model(inputs=[input_1,input_2], outputs=x)
   return model
 
 model = LAS(256, 512, 16)
